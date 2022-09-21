cff144e759386c502f7aa5ff0e75f0c347929dd7
Emirokut <epicw70@gmail.com>
2022-09-21 19:58:45 +0300

    ilk commit

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,23 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.55;
+		sayi =127;
+		
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,28 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// For
+		for (int i = 2; i < 10; i += 2) {
+			System.out.println(i);
+
+		}
+		System.out.println("for döngüsü bitti");
+
+		// While
+
+		int i = 1;
+
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+		}
+
+		System.out.println("while döngüsü bitti");
+
+		// Do While
+
+	}
+
+}
